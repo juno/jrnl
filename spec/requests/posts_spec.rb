b403925ae7dd9fbc6af2eb506b37cccdb440e9a3
Junya Ogura <junyaogura@gmail.com>
2011-01-06 18:29:08 +0900

    updated specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe "Posts" do
+  describe "GET /" do
+    it "works!" do
+    end
+  end
+
   describe "GET /posts" do
     it "works! (now write some real specs)" do
       get posts_path
