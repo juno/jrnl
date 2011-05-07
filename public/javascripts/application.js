77bd7be188a064860e136a3f275ade30df0d7385
Junya Ogura <junyaogura@gmail.com>
2011-05-08 00:36:09 +0900

    add Mixpanel tracking code.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,4 +5,6 @@ $(document).ready(function() {
   });
 
   $('#content article.post .content a:not(.noembed)').embedly({'maxWidth': 500, 'method':'replace', 'wrapElement':'div', 'className':'embedded'});
+
+  mpmetrics.track('page-view', {'location' : location.href});
 });
