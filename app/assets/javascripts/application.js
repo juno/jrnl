431f16cdc348086f213112b0d9cc6bb885d3643e
Junya Ogura <junyaogura@gmail.com>
2013-01-11 21:30:54 +0900

    cleanup configurations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.embedly.min
-//= require_self
 //= require_tree .
-
-/* rest of file omitted */
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,34 +11,61 @@ Jrnl::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
+  config.static_cache_control = 'public, max-age=2592000'
+  config.assets.digest = true
+
+  # アセットを圧縮する
+  config.assets.compress = true
+
+  # プリコンパイルされたアセットが見つからない場合にアセットパイプラインへのフォールバックをする
+  config.assets.compile = true
+
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
+
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  # Configure the sendfile headers for Heroku.  "X-Accel-Redirect" is also a good value for this since Heroku use Nginx.
+  config.action_dispatch.x_sendfile_header = nil
 
-  # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-  # If you have no front-end server that supports something like X-Sendfile,
-  # just comment this out and Rails will serve the files
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
+  # Prepend all log lines with the following tags
+  # config.log_tags = [ :subdomain, :uuid ]
+
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
-
-  # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.default_url_options = { :host => 'journal.sooey.com' }
+
+  # Heroku Cedar Stack + Sendgridの場合に必要な設定
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '25',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['SENDGRID_DOMAIN'],
+  }
 
   # Enable threaded mode
   # config.threadsafe!
@@ -50,16 +77,7 @@ Jrnl::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
-
-  # アセットを圧縮する
-  config.assets.compress = true
-
-  # プリコンパイルされたアセットが見つからない場合にアセットパイプラインへのフォールバックをする
-  config.assets.compile = false
-
-  # アセットURL用にダイジェストを生成する
-  config.assets.digest = true
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
