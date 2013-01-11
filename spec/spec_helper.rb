e92d9e5bb299e3f0a2982845c4e2cdaa948febf7
Junya Ogura <junyaogura@gmail.com>
2013-01-11 21:19:33 +0900

    update simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,9 @@
 ENV["RAILS_ENV"] ||= 'test'
 
 require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '.bundle/'
-  add_filter 'vendor/bundles/'
-end
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
