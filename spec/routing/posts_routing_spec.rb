7b40756af009cf1a511dbce456e9577641a850b4
Junya Ogura <junyaogura@gmail.com>
2013-07-23 16:31:50 +0900

    Refactor specs

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,41 +1,41 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe PostsController do
 
   describe 'GET /posts' do
-    it { { :get => "/posts" }.should route_to(:controller => "posts", :action => "index") }
+    it { { get: '/posts' }.should route_to(controller: 'posts', action: 'index') }
   end
 
   describe 'GET /posts/new' do
-    it { { :get => "/posts/new" }.should route_to(:controller => "posts", :action => "new") }
+    it { { get: '/posts/new' }.should route_to(controller: 'posts', action: 'new') }
   end
 
   describe 'GET /posts/1' do
-    it { { :get => "/posts/1" }.should route_to(:controller => "posts", :action => "show", :id => "1") }
+    it { { get: '/posts/1' }.should route_to(controller: 'posts', action: 'show', id: '1') }
   end
 
   describe 'GET /posts/1/edit' do
-    it { { :get => "/posts/1/edit" }.should route_to(:controller => "posts", :action => "edit", :id => "1") }
+    it { { get: '/posts/1/edit' }.should route_to(controller: 'posts', action: 'edit', id: '1') }
   end
 
   describe 'POST /posts' do
-    it { { :post => "/posts" }.should route_to(:controller => "posts", :action => "create") }
+    it { { post: '/posts' }.should route_to(controller: 'posts', action: 'create') }
   end
 
   describe 'PUT /posts/1' do
-    it { { :put => "/posts/1" }.should route_to(:controller => "posts", :action => "update", :id => "1") }
+    it { { put: '/posts/1' }.should route_to(controller: 'posts', action: 'update', id: '1') }
   end
 
   describe 'DELETE /posts/1' do
-    it { { :delete => "/posts/1" }.should route_to(:controller => "posts", :action => "destroy", :id => "1") }
+    it { { delete: '/posts/1' }.should route_to(controller: 'posts', action: 'destroy', id: '1') }
   end
 
   describe 'GET /1' do
-    it { { :get => "/1" }.should route_to(:controller => "posts", :action => "show", :id => "1") }
+    it { { get: '/1' }.should route_to(controller: 'posts', action: 'show', id: '1') }
   end
 
   describe 'GET /archives/2011/1' do
-    it { { :get => "/archives/2011/1" }.should route_to(:controller => "posts", :action => "monthly_archive", :year => "2011", :month => "1") }
+    it { { get: '/archives/2011/1' }.should route_to(controller: 'posts', action: 'monthly_archive', year: '2011', month: '1') }
   end
 
 end
