6692dcd3d02d5a55c741f0b073b20f0ab6be8ea3
Junya Ogura <junyaogura@gmail.com>
2014-04-17 09:07:39 +0900

    Add ActionMailer configurations for Mandrill on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'journal.sooey.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    port:           '587',
+    address:        'smtp.mandrillapp.com',
+    user_name:      ENV['MANDRILL_USERNAME'],
+    password:       ENV['MANDRILL_APIKEY'],
+    domain:         'heroku.com',
+    authentication: :plain
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
