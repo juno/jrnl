ed344b7a12ac357d8fdd9b6b59314178e6ccdd57
Junya Ogura <juno@sooey.com>
2022-02-08 17:46:42 +0900

    chore: Add IaC config for render deployment

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+
+if [ "${RENDER_SERVICE_TYPE}" = "web" ]; then
+  bundle exec rake assets:precompile
+  bundle exec rake assets:clean
+  bundle exec rake db:migrate
+  bundle exec rake db:seed
+fi
