e5d55b0534826af0e09ebdf8a5fe69f642049199
Junya Ogura <junya@sooey.com>
2024-12-01 23:15:35 +0900

    chore: Update rails defaults to 8.0

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Jrnl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
 
     config.time_zone = "Tokyo"
 
